166f74ab7eaf0726aff674d2c10441be3fe3dc79
Alin Ilea <roxana.opris@evozon.com>
2017-05-25 15:03:07 +0200

    Scroll-top button will now be hidden on the first section of the page.

--- app/components/scroll-btn.js
+++ app/components/scroll-btn.js
@@ -1,53 +1,75 @@
+import { Component } from 'react'
 import { style, mediaQueries } from '../constants'
 
-const ScrollBtn = props => (
-  <div>
-    <div id="back_top">
-      <a href="#home">
-        <i className="fa fa-angle-up" />
-      </a>
+class ScrollBtn extends Component {
+  componentDidMount() {
+    window.onscroll = function(){
+      const button = document.getElementById('back_top')
+      if (!button.classList.contains("hide") && document.body.scrollTop < 900) {
+        button.classList.add('hide')
+      }
+      else if (button.classList.contains("hide") && document.body.scrollTop >= 900) {
+        button.classList.remove('hide')
+      }
+    }
+    window.onscroll()
+  }
+
+  render() {
+    return (
+
+    <div>
+      <div id="back_top">
+        <a href="#home">
+          <i className="fa fa-angle-up" />
+        </a>
+      </div>
+      <style jsx>{`
+        #back_top {
+          bottom: 20px;
+          position: fixed;
+          right: 30px;
+          z-index: 3;
+          box-sizing: border-box;
+          display: block;
+        }
+        #back_top a {
+          background: #000 none repeat scroll 0 0;
+          border-radius: 0;
+          color: #fff;
+          display: block;
+          font-size: 31px;
+          height: 52px;
+          line-height: 52px;
+          text-align: center;
+          vertical-align: top;
+          width: 52px;
+          text-decoration: none;
+          box-sizing: border-box;
+          opacity: 1;
+          transition: opacity .4s;
+        }
+        #back_top a:hover {
+          opacity: .57;
+          color: #228dcb;
+          fill: #228dcb;
+          text-decoration: none;
+          outline: 0;
+        }
+        #back_top i {
+          display: inline-block;
+          font: normal normal normal 14px/1 FontAwesome;
+          font-size: inherit;
+          text-rendering: auto;
+          -webkit-font-smoothing: antialiased;
+        }
+        #back_top.hide a {
+          opacity: 0;
+        }
+      `}</style>
     </div>
-    <style jsx>{`
-      #back_top {
-        bottom: 20px;
-        position: fixed;
-        right: 30px;
-        z-index: 3;
-        box-sizing: border-box;
-        display: block;
-      }
-      #back_top a {
-        background: #000 none repeat scroll 0 0;
-        border-radius: 0;
-        color: #fff;
-        display: block;
-        font-size: 31px;
-        height: 52px;
-        line-height: 52px;
-        text-align: center;
-        vertical-align: top;
-        width: 52px;
-        text-decoration: none;
-        box-sizing: border-box;
-        opacity: 1;
-        transition: opacity .4s;
-      }
-      #back_top a:hover {
-        opacity: .57;
-        color: #228dcb;
-        fill: #228dcb;
-        text-decoration: none;
-        outline: 0;
-      }
-      #back_top i {
-        display: inline-block;
-        font: normal normal normal 14px/1 FontAwesome;
-        font-size: inherit;
-        text-rendering: auto;
-        -webkit-font-smoothing: antialiased;
-      }
-    `}</style>
-  </div>
-)
+    )
+  }
+}
 
 module.exports = ScrollBtn
